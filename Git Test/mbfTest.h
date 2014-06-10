4769f83a64dc385d2575ed3f5281a9b6ffdb40f5
Ian Reid <Ian@Ians-MacBook-Air.local>
2014-06-10 15:40:01 -0400

    added a test class

---
+++ Git Test/mbfTest.h
@@ -0,0 +1,13 @@
+//
+//  mbfTest.h
+//  Git Test
+//
+//  Created by Ian Reid on 2014-06-10.
+//  Copyright (c) 2014 Ian Reid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mbfTest : NSObject
+
+@end
---
+++ Git Test/mbfTest.m
@@ -0,0 +1,13 @@
+//
+//  mbfTest.m
+//  Git Test
+//
+//  Created by Ian Reid on 2014-06-10.
+//  Copyright (c) 2014 Ian Reid. All rights reserved.
+//
+
+#import "mbfTest.h"
+
+@implementation mbfTest
+
+@end
